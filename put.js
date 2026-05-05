00fac536907eb0a2bc4568e8d7cb71dd40bf5b6b
putcode677 <putcode701@gmail.com>
2026-05-06 02:10:37 +0300

    console.table

---
+++ put.js
@@ -0,0 +1,7 @@
+const data = [
+    { registration_number: "T25_03_2170", first_name: "john", last_name: "doe" },
+    { registration_number: "T25_03_2171 ", first_name: "hamisi", last_name: "martin" },
+    { registration_number: "T25_03_2172", first_name: "jane", last_name: "smith" }
+];
+
+console.table(data);
---
+++ script.js
@@ -0,0 +1,28 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let taskText = input.value;
+
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.onclick = function () {
+    li.style.textDecoration = "line-through";
+  };
+
+  
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(deleteBtn);
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
